d84f66df93177adae90112592730240ad22f54e6
Adonai <adonairc@gmail.com>
2024-11-03 21:56:33 -0800

    pulse sequences test

--- test/test.py
+++ test/test.py
@@ -14,39 +14,27 @@ async def test_project(dut):
     clock = Clock(dut.clk, 10, units="ns")
     cocotb.start_soon(clock.start())
 
-    # Reset
-    dut._log.info("Reset")
-    dut.ena.value = 1
-    dut.uio_in.value = 0
-    dut.rst_n.value = 0
+   
+    widths = [100,200,150,250,300,50,400,100,500,125,350,275,450,150,225,275]
+    periods = [1000,1200,1100,1500,2000,800,2500,900,3000,1000,2200,1800,2800,1300,1700,1900]
+    counts = [10,8,12,5,15,20,7,10,4,14,6,9,3,11,13,16]
 
 
-    dut._log.info("Test pulse sequence #0")
-    dut.ui_in.value = 0
+    # Test sequences
+    for sequence in range(len(widths)):
+        dut._log.info(f"Testing pulse sequence #{sequence}")
+         # Reset
+        dut.ena.value = 1
+        dut.rst_n.value = 0
+        await ClockCycles(dut.clk, 1)
 
-    # Wait for one clock cycle to reg input
-    await ClockCycles(dut.clk, 1)
-    print("Output at clk #1 = ",dut.uo_out.value)
-    # pulse_width[0] = 32'd100;
-    # pulse_period[0] = 32'd1000;
-    # pulse_count[0] = 16'd10;
+        dut.rst_n.value = 1
+        dut.ui_in.value = sequence
 
-
-    await ClockCycles(dut.clk, 100)
-    print("Output at clk #100 = ",dut.uo_out.value)
-    # dut.rst_n.value = 1
-
-
-    # Set the input values you want to test
-    # dut.ui_in.value = 20
-    # dut.uio_in.value = 30
-
-    # Wait for one clock cycle to see the output values
-    # await ClockCycles(dut.clk, 1)
-
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    # assert dut.uo_out.value == 50
-
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+        await ClockCycles(dut.clk, 2)
+        assert dut.uo_out.value == 0
+      
+        # Wait for one clock cycle to reg input
+        await ClockCycles(dut.clk, widths[sequence])
+        assert dut.uo_out.value == 1
+        
