3bf994247cf391c1b8dbdc0406f29eab75041424
Adonai <adonairc@gmail.com>
2024-11-03 21:03:44 -0800

    test pulse sequence #0

--- test/test.py
+++ test/test.py
@@ -10,27 +10,39 @@ from cocotb.triggers import ClockCycles
 async def test_project(dut):
     dut._log.info("Start")
 
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, units="us")
+    # Set the clock period to 10 ns (100 MHz)
+    clock = Clock(dut.clk, 10, units="ns")
     cocotb.start_soon(clock.start())
 
     # Reset
     dut._log.info("Reset")
     dut.ena.value = 1
-    dut.ui_in.value = 0
     dut.uio_in.value = 0
     dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
 
-    dut._log.info("Test project behavior")
+
+    dut._log.info("Test pulse sequence #0")
+    dut.ui_in.value = 0
+
+    # Wait for one clock cycle to reg input
+    await ClockCycles(dut.clk, 1)
+    print("Output at clk #1 = ",dut.uo_out.value)
+    # pulse_width[0] = 32'd100;
+    # pulse_period[0] = 32'd1000;
+    # pulse_count[0] = 16'd10;
+
+
+    await ClockCycles(dut.clk, 100)
+    print("Output at clk #100 = ",dut.uo_out.value)
+    # dut.rst_n.value = 1
+
 
     # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    # dut.ui_in.value = 20
+    # dut.uio_in.value = 30
 
     # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
+    # await ClockCycles(dut.clk, 1)
 
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
